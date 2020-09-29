ae84363470cee503a8fd6665d9ee48f41fcc90df
Le Bin Ho <bin262@gmail.com>
2020-09-29 14:06:45 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# tqix
+ >>> tqix: a Toolbox for Quantum in X:
+ >>>    x: quantum measurement, quantum metrology, quantum tomography, and more.
+
+# description
+ >>> tqix is open-source software providing some convenient tools 
+     for quantum measurement, quantum metrology, quantum tomography and more.
+
+# license
+ >>> copyright (c) 2019 and later\
+ >>> authors: Le Bin Ho\
+ >>> all rights reserved.
+
+# note for installation:
+ >>> download source code and run:
+ >>> $sudo pip3 install .
---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+tqix: a Toolbox for Quantum in X:
+   x: quantum measurement, quantum metrology, quantum tomography, and more.
+"""
+import os
+import sys
+
+from setuptools import setup
+
+# all information about tqix is here
+MAN = 1
+SUB = 0
+SUBS= 1
+VERSION = '%d.%d.%d' % (MAN,SUB,SUBS)
+REQUIRES = ['numpy (>=1.8)', 'scipy (>=0.15)', 'cython (>=0.21)']
+PACKAGES = ['tqix', 'tqix/dsm', 'tqix/povm']
+
+NAME = "tqix"
+AUTHOR = ("Binho Le")
+AUTHOR_EMAIL = ("binho@kindai.ac.jp")
+LICENSE = "GNU"
+DESCRIPTION = "A Toolbox for Quantum in X"
+KEYWORDS = "quantum measurement, quantum metrology, quantum tomography"
+URL = "http://"
+PLATFORMS = ["Linux", "Mac OSX", "Unix", "Windows"]
+
+def write_version_py(filename='tqix/version.py'):
+    cnt = """\
+#this file is generated from tqix setup.py
+version = '%(version)s'
+"""
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION})
+    finally:
+        a.close()
+
+# if exists then remove
+if os.path.exists('tqix/version.py'):
+    os.remove('tqix/version.py')
+
+
+
+write_version_py()
+
+setup(name = NAME,
+      version = VERSION,
+      description=DESCRIPTION,
+      url=URL,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      license=LICENSE,
+      packages=PACKAGES,
+      zip_safe=False)
+
+
+#find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
