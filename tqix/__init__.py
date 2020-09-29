cd73d41a4c8da734f9ee46b96224cd1b39d7cdd5
Le Bin Ho <bin262@gmail.com>
2020-09-29 14:13:03 +0900

    Create __init__.py

---
+++ tqix/__init__.py
@@ -0,0 +1,47 @@
+"""
+>>> this file is a part of tqix: a Toolbox for Quantum in X
+                              x: quantum measurement, quantum metrology, 
+                                 quantum tomography, and more.
+________________________________
+>>> copyright (c) 2019 and later
+>>> authors: Binho Le
+>>> all rights reserved
+________________________________
+"""
+
+import os
+import sys
+import warnings
+
+import tqix.version
+from tqix.version import version as __version__
+
+# ------------------------------------------
+# Load modules
+# ==========================================
+
+# utility
+from tqix.utility import *
+from tqix.about import *
+
+# outer loop
+from tqix.qstate import *
+from tqix.qx import *
+from tqix.qmeas import *
+from tqix.qoper import *
+from tqix.qtool import *
+from tqix.backend import *
+
+from tqix.quasi_prob import *
+from tqix.visualize import *
+
+# quantum state tomography
+from tqix.dsm import *
+
+# povms
+from tqix.povm import *
+
+# -----------------------------------------------------------------------------
+# Clean name space
+#
+del os, sys#, numpy, scipy, multiprocessing, distutils
