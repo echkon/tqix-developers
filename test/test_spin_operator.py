1e248a3b68a36d40d147051359dde84cd8974e04
viet <vietnthe153763@fpt.edu.vn>
2022-02-07 09:45:51 +0700

    add spin operators

---
+++ test/test_spin_operator.py
@@ -0,0 +1,10 @@
+from tqix import *
+import numpy as np
+from tqix.pis import *
+
+print(Sx(1/2).toarray())
+print(Sy(1/2).toarray())
+print(Sz(1/2).toarray())
+print(S_minus(1/2).toarray())
+print(S_plus(1/2).toarray())
+print(S_2(1/2).toarray())
