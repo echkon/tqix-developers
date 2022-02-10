588e3c307578105e0ffe72e3916a9a3db62cf6e4
viet <vietnthe153763@fpt.edu.vn>
2022-02-10 23:34:00 +0700

    add rz gate

--- tqix/pis/gates.py
+++ tqix/pis/gates.py
@@ -13,10 +13,14 @@ ________________________________
 #from numpy import
 import numpy as np
 import cmath as cm
+
+from sympy import csc
 from tqix.qx import *
 from tqix.qtool import dotx
 from tqix.pis.util import *
 from tqix.pis import *
+from scipy.sparse import bsr_matrix,block_diag,csc_matrix
+from scipy.sparse.linalg import expm
 
 __all__ = ['RZ']
 
@@ -34,7 +38,7 @@ def RZ(sobj,theta):
     new state
     """
 
-    state = sobj.state
+    state = sobj.state.toarray()
     d = shapex(state)[0]
     Nds = get_Nds(d) #cannot use for pure
     Nin = sobj.N #get from input
@@ -45,7 +49,6 @@ def RZ(sobj,theta):
         for idx in np.nonzero(state)[0]:
             m = int(j - idx)
             new_state[idx,0] = cm.exp(-1j*theta*m)*state[idx,0]
-        sobj.state = new_state
     else:
         if Nin != Nds: #not full blocks
            j = Nin/2
@@ -55,7 +58,6 @@ def RZ(sobj,theta):
            for ik in iks:
               (m, m1) = mm1[ik]
               new_state[ik] = state[ik]*cm.exp(-1j*theta*(m-m1)) 
-           sobj.state = new_state
         else: #full blocks
            new_state = np.zeros((d,d),dtype = complex)
            iks = _get_non0_idx(state)
@@ -63,7 +65,7 @@ def RZ(sobj,theta):
            for ik in iks:
                (j,m,m1) = jmm1[ik]
                new_state[ik] = state[ik]*cm.exp(-1j*theta*(m-m1))
-           sobj.state = new_state
+    sobj.state = bsr_matrix(new_state)
 
 def _get_non0_idx(matrix):
     """get non zero indexs of a matrix"""
@@ -73,3 +75,46 @@ def _get_non0_idx(matrix):
           if l != 0.0:
              lidx.append((i,k))
     return lidx
+
+def RZ_sparse(sobj,theta):
+    state = sobj.state
+    d_in = shapex(state)[0]
+    N_in = sobj.N
+    d_dicke = get_dim(N_in)
+
+    if d_in != d_dicke:
+        J_z = Sz(N_in/2)
+
+    else:
+        j_array = get_jarray(N_in)[::-1]
+        blocks = []
+        for j in j_array:
+            blocks.append(Sz(j))
+        J_z = block_diag(blocks)
+
+    J_z = csc_matrix(J_z)
+    expJ = expm(-1j*theta*J_z)
+    expJ_conj = csc_matrix(daggx(expJ))
+    new_state = expJ.dot(sobj.state).dot(expJ_conj)
+
+    sobj.state = new_state
+ 
+if __name__ == "__main__":
+    N=3
+
+    print('test circuit')
+    qc = circuit(N)
+    state = qc.state
+    print(state)
+    print(typex(state))
+
+    print('test initial state')
+    init_state = dotx(state,daggx(state))
+    qc = circuit(N,init_state)
+    state = qc.state
+    print(state)
+    print(typex(state))
+
+    print('test gate with initial state')
+    RZ(qc,np.pi/3)
+    print(qc.state)
