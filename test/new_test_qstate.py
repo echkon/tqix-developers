94185ed1ce20c8f38dfc4dc6fea4a96b6aa7597a
viet <vietnthe153763@fpt.edu.vn>
2022-03-09 18:07:45 +0700

    add F

--- test/new_test_qstate.py
+++ test/new_test_qstate.py
@@ -9,11 +9,12 @@ qc = circuit(N)
 THETA = [0, np.pi]
 PHI = [0, 2* np.pi]
 # husimi_spin_3d(qc.state.toarray(), THETA ,PHI ,cmap = cmindex(1),fname ="husimi_sphere.eps",alpha=0.5,view=(-90,0))
-qc.OAT(np.pi/70,"X")
+qc.OAT(0,"X")
 print(qc.state.diagonal().sum())
 print("xi_2_H:",get_xi_2_H("x","y","z",qc))
 print("xi_2_S:",get_xi_2_S(qc))
 print("xi_2_R:",get_xi_2_R(qc))
 print("xi_2_D:",get_xi_2_D(qc,n=[1,0,0]))
 print("xi_2_E:",get_xi_2_E(qc,n=[1,0,0]))
+print("xi_2_F:",get_xi_2_F(qc,[1,0,0],[0,1,0],[0,0,1]))
 # husimi_spin_3d(qc.state.toarray(), THETA ,PHI ,cmap = cmindex(1),fname ="afterhusimi_sphere.eps",alpha=0.5,view=(-90,0))
--- tqix/pis/squeeze_param.py
+++ tqix/pis/squeeze_param.py
@@ -2,7 +2,7 @@ from tqix.pis.util import *
 from tqix.pis import *
 import numpy as np 
 
-__all__ = ['get_xi_2_H','get_xi_2_S','get_xi_2_R','get_xi_2_D','get_xi_2_E']
+__all__ = ['get_xi_2_H','get_xi_2_S','get_xi_2_R','get_xi_2_D','get_xi_2_E','get_xi_2_F']
 
 def get_xi_2_H(alpha,beta,gamma,qc):
     var_alpha = qc.var(alpha)
@@ -17,22 +17,31 @@ def get_xi_2_S(qc):
     return 2/N*(mean_Jx2_Jy2-np.abs(mean_J_2_minus))
 
 def get_xi_2_R(qc):
-    xi_2_S = get_xi_2_S(qc)
     N = qc.N
+    xi_2_S = get_xi_2_S(qc)
     mean_Jz = qc.expval(type="z")
     return (N**2/(4*np.abs(mean_Jz)**2))*xi_2_S
 
 def get_xi_2_D(qc,n):
-    var = qc.var(type="xyz",use_vector=True,n=n)
-    mean = qc.expval(type="xyz",use_vector=True,n=n)
     N = qc.N
+    var = qc.var(type="xyz",use_vector=True,n=n)
+    mean = qc.expval(type="xyz",use_vector=True,n=n)   
     return (N*var)/(N**2/4-mean**2)
 
 def get_xi_2_E(qc,n):
+    N = qc.N
     var_Jvec = qc.var(type="xyz",use_vector=True,n=n)
     mean_Jvec = qc.expval(type="xyz",use_vector=True,n=n)
-    N = qc.N
     mean_J_2 = qc.expval(type="J2")
     return (N*var_Jvec)/(mean_J_2-N/2-mean_Jvec**2)
+
+def get_xi_2_F(qc,n1,n2,n3):
+    N=qc.N
+    var_J_n1 = qc.var(type="xyz",use_vector=True,n=n1)
+    mean_J_n2 = qc.expval(type="xyz",use_vector=True,n=n2)
+    mean_J_n3 = qc.expval(type="xyz",use_vector=True,n=n3)
+    return (N*var_J_n1)/(mean_J_n2**2+mean_J_n3**2)
+
+
 
 
