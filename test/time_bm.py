9f642a5e2247df3c43442d54e6dd74518967af65
viet <vietnthe153763@fpt.edu.vn>
2022-11-01 08:26:24 +0700

    fix import

--- test/time_bm.py
+++ test/time_bm.py
@@ -6,24 +6,24 @@ import matplotlib.pyplot as plt
 N_max=200 #max number of qubits
 
 #run time benchmark on noiseless qubit system
-noiseless_time_qubits = []
-for N in range(N_max):
-    qc = circuit(N)
+# noiseless_time_qubits = []
+# for N in range(N_max):
+#     qc = circuit(N)
 
-    start = time.time()
+#     start = time.time()
 
-    qc.RX(np.pi/3)
-    qc.RY(np.pi/3)
-    qc.RZ(np.pi/3)
+#     qc.RX(np.pi/3)
+#     qc.RY(np.pi/3)
+#     qc.RZ(np.pi/3)
 
-    noiseless_time = time.time()-start
-    noiseless_time_qubits.append(noiseless_time)
+#     noiseless_time = time.time()-start
+#     noiseless_time_qubits.append(noiseless_time)
 
 #run time benchmark on noise qubit system with 1 process for simulating noise
 noise_time_qubits = []
-for N in range(N_max):
+for N in range(1,N_max):
     qc = circuit(N)
-
+    print(N)
     start = time.time()
 
     qc.RX(np.pi/3,noise=0.05)
@@ -63,9 +63,8 @@ for N in range(N_max):
     gpu_noise_time_qubits.append(gpu_noise_time)
 
 #plot data
-plt.plot(range(noiseless_time_qubits), noiseless_time_qubits, label = "noiseless")
-plt.plot(range(noise_time_qubits), noise_time_qubits, label = "noise (#process = 1)")
-plt.plot(range(mp_noise_time_qubits), mp_noise_time_qubits, label = "noise (#process = 25)")
-plt.plot(range(gpu_noise_time_qubits), gpu_noise_time_qubits, label = "noise (GPU: NVIDIA V100)")
+plt.plot(range(len(noise_time_qubits)), noise_time_qubits, label = "noise (#process = 1)")
+plt.plot(range(len(mp_noise_time_qubits)), mp_noise_time_qubits, label = "noise (#process = 25)")
+plt.plot(range(len(gpu_noise_time_qubits)), gpu_noise_time_qubits, label = "noise (GPU: NVIDIA V100)")
 plt.legend()
 plt.show()
