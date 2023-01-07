d23a1ef7b60c8d3b91fd740bb7c54cf98371c1c4
vietnguyen012 <vietnthe1153763@fpt.edu.vn>
2023-01-07 13:35:59 +0900

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,11 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
+html_static_path = ['_static']
+html_logo = "logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
