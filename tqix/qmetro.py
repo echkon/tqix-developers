f42dae9debeff832148f9b120aa4058d7f8f05cb
Binho <bin262@gmail.com>
2023-07-25 09:27:22 +0900

    add qfim

--- tqix/qmetro.py
+++ tqix/qmetro.py
@@ -10,22 +10,51 @@ ________________________________
 """
 # for quantum metrology
 
-__all__ = ['qfimx', 'Hx', 'Ux']
+__all__ = ['qfimx', 'Hx', 'Ux', 'Ax']
 
 import numpy as np
+from tqix.qx import daggx, dotx, eigenx
 from scipy.linalg import expm,inv
 
-def qfimx(state,H):
+def qfimx(inp_state,h_opt,c_opt,t):
     """calculate quantum fisher information
 
     Args:
-        state (matrix): quantum state (density matrix)
-        H (function): Hamiltonian
+        inp_state (matrix): input quantum state 
+        h_opt(list): list of Hamiltonian
+        c_opt(list): list of coefficients
+        t(float): time
         
     Returns:
         qfim: quantum fisher information matrix    
     """
-    return 1
+    
+    # length of parameters
+    d = len(h_opt)
+    
+    # unitary, #Ax #final state
+    uni = Ux(h_opt,c_opt,t)
+    ax = Ax(h_opt,c_opt,t)
+    fin_state = dotx(uni,inp_state,daggx(uni))
+    
+    # eigen_rho,  # derivative of fin_state
+    eigens = eigenx(fin_state)
+    drhos = _droh(fin_state,uni,ax)
+    
+    # qfim
+    for k in range(d):
+        for l in range(d):
+            H[k,l] = 0.0
+            for i in range(len(eigens[0])):
+                for j in range(len(eigens[0])):
+                    de = eigens[0][i] + eigens[0][j]
+                    if de > 10e-15:
+                        num1 = dotx(daggx(eigens[1][i]),drhos[k],eigens[1][j])
+                        num2 = dotx(daggx(eigens[1][j]),drhos[l],eigens[1][i])
+                        H[k,l] += 2*np.real(num1*num2)/de
+                  
+    return np.real(H)     
+    
 
 def Hx(h_opt,c_opt):
     """to create Hamiltonian from h_opt and c_opt
@@ -61,20 +90,41 @@ def Ux(h_opt,c_opt,t):
 ###
 #def _drho():
     
-def _integrate(A,H,t):
-    """calculate integrate exp(itH)@A@exp(-itH)
+def Ax(h_opt,c_opt,t):
+    """calculate Ax = exp(itH)@A@exp(-itH)
 
     Args:
-        A (matrix): Hermitian operator
-        H (matrix): Hamiltonian
+        h_opt (list): list of Hamiltonian
+        c_opt (list): list of coefficients
         t (float): time
 
     Returns:
-        Ak: Hermition opetertor Ak = int_0^t exp(itH)@A@exp(-itH)
+        Ax: list of Ax
     """
-    f = lambda u: dotx(expm(1j*u*H),A,expm(-1j*u*H))
+    if len(h_opt) != len(c_opt):
+        raise TypeError("list length does not match")
+    
+    axl = []
+    hamil = Hx(h_opt,c_opt)
     xv = np.linspace(0,t,1000)
-    result = np.apply_along_axis(f,0,xv.reshape(1,-1))
-    return np.trapz(result,xv)
+    for i in range(len(h_opt)):
+        f = lambda u: dotx(expm(1j*u*hamil),h_opt[i],expm(-1j*u*hamil))
+        result = np.apply_along_axis(f,0,xv.reshape(1,-1))
+        axl.append(np.trapz(result,xv))
+    return axl #list of Ax    
 
+def _droh(rho,U,A):
+    """calculate d_rho following Eq.(C1) PRA 102, 022602 (2020)
 
+    Args:
+        rho (matrix): quantum state need to calculate deri
+        U (matrix): unitary
+        A (list): list of Ak
+    """
+    drhos = []
+    for i in range(len(A)):
+        dU = -1j*dotx(U,Ak[i])
+        dUU = dotx(dU,rho,daggx(U))
+        UdU = dotx(U,rho,daggx(dU))
+        drhos.append(dUU + UdU)
+    return drhos    
